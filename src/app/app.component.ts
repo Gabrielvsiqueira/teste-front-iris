814afd21418ae6a0400b665e1ee4964e60bf46ea
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-09-23 15:02:38 -0300

    refactor: add test project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListFormsComponent } from './components/list-forms/list-forms.component';
+import { ListUsersComponent } from './components/list-users/list-users.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListFormsComponent, ListUsersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ListUsersComponent } from './components/list-users/list-users.component';
+import { ListFormsComponent } from './components/list-forms/list-forms.component';
 
-export const routes: Routes = [];
+export const routes: Routes =
+[
+
+];
